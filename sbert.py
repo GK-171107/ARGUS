4f0303c950273aa79faab3f2aeb06aad2a486280
GK171107 <gk.proj2007@gmail.com>
2026-05-16 22:07:46 +0530

    Embeddings appended

--- sbert.py
+++ sbert.py
@@ -1,18 +1,21 @@
+import numpy as np
 from sentence_transformers import SentenceTransformer
 import json
-
-sentences = []
-model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-
 
 with open("thread_comments.json", 'r', encoding='utf-8') as f:
     comments = json.load(f)
 
-for comment in comments[:10]:
-    sentences.append(comment["body"])
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-embeddings = model.encode(sentences)
-print(embeddings.shape)
 
-similarities = model.similarity(embeddings, embeddings)
-print(similarities)
+def attach_embeddings(comment):
+    comment['embedding'] = model.encode(comment['body']).tolist()
+
+    for reply in comment.get('replies', []):
+        attach_embeddings(reply)
+
+for comment in comments:
+    attach_embeddings(comment)
+
+with open("thread_embeddings.json", 'w', encoding='utf-8') as f:
+    json.dump(comments, f, ensure_ascii=False, indent=4)
